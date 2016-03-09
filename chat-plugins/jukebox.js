315970ac386b5b18d6b76c42a9c47f08bac89794
ashyudhajit <yagnaseni5@gmail.com>
2016-03-09 20:08:22 +0530

    Update jukebox.js

--- chat-plugins/jukebox.js
+++ chat-plugins/jukebox.js
@@ -2,10 +2,7 @@
   * parses links into the HTML for music boxes
   * by Haji
   */
- 
- var https = require("https");
  var request = require('request');
- var title = "";
  
  exports.commands = {
      jb: 'jukebox',
@@ -28,7 +25,7 @@
          return new Promise(function(resolve, reject) {
              request(link, function(err, res, body) {
                 if (!err && res.statusCode == 200) {
-                     var str = '<a href="' + link + '"><button title="' + title + '">' + title + '</button></a><br />'; //parse it now
+                     var str = '<a href="' + link + '"></a><br />'; //parse it now
                      resolve(str);
                 } else {
                      reject(str);
